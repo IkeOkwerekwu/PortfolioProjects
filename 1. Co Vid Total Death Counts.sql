f38555775099e7653d210bac1332bf3c27cd92a9
IkeOkwerekwu <100385254+IkeOkwerekwu@users.noreply.github.com>
2022-02-24 22:01:50 -0500

    Delete 1. Co Vid Total Death Counts.sql

--- 1. Co Vid Total Death Counts.sql
+++
@@ -1,6 +0,0 @@
-Select location, SUM(cast(new_deaths as float)) as TotalDeathCount
-From Covid.CovidDeath
-Where continent is not null 
-and location not in ('World', 'European Union', 'International')
-Group by location
-order by TotalDeathCount desc
