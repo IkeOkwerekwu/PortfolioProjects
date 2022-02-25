55e9a9127165b4272b6e825292dc67e9381064f4
IkeOkwerekwu <100385254+IkeOkwerekwu@users.noreply.github.com>
2022-02-24 22:02:07 -0500

    Delete 3.Co Vid Infection Rates.sql

--- 3.Co Vid Infection Rates.sql
+++
@@ -1,5 +0,0 @@
-SELECT Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From Covid.CovidDeath
-WHERE continent is not null
-GROUP BY Location, Population
-ORDER BY PercentPopulationInfected desc
