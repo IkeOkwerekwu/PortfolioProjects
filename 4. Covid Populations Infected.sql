283fc465dcd4fb1bcd0e44f45a0843b77ea26795
IkeOkwerekwu <100385254+IkeOkwerekwu@users.noreply.github.com>
2022-02-24 22:05:39 -0500

    Delete 4. Covid Populations Infected.sql

--- 4. Covid Populations Infected.sql
+++
@@ -1,5 +0,0 @@
-SELECT Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-FROM Covid.CovidDeath
-WHERE continent is not null
-GROUP BY Location, Population, date
-ORDER BY PercentPopulationInfected desc
