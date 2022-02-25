f1113ba1d1f532bf023be20943869974c3640967
IkeOkwerekwu <100385254+IkeOkwerekwu@users.noreply.github.com>
2022-02-24 22:05:27 -0500

    Delete 2. Co Vid Death Percentages.sql

--- 2. Co Vid Death Percentages.sql
+++
@@ -1,5 +0,0 @@
-SELECT Location, Date, SUM(new_cases) as total_cases, SUM(cast(new_deaths as float)) as total_deaths, SUM(cast(new_deaths as float))/SUM(New_Cases)*100 as DeathPercentage
-FROM covid.coviddeath
-WHERE continent is not null
-GROUP By location, date
-ORDER BY 1,2
